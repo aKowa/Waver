ad9e96ab1c32435372fd9df0ceb9f497448b8ab5
Andre Kowalewski <a.kowa@gmx.de>
2016-04-26 22:46:35 +0200

    TESTED EventManager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+	private static EventManager s_Instance = null;
+
+	public static EventManager Instance
+	{
+		get
+		{
+			if (s_Instance == null)
+			{
+				s_Instance = GameObject.FindObjectOfType(typeof(EventManager)) as EventManager;
+			}
+
+			return s_Instance;
+		}
+	}
+
+	public delegate void EventDelegate<T> (T e) where T : GameEvent;
+	private delegate void EventDelegate (GameEvent e);
+}
