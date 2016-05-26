ec7c5682df81337eaed359040e257c4b8fd38348
Andre Kowalewski <a.kowa@gmx.de>
2016-05-26 17:08:42 +0200

    minor changes

--- Assets/Scripts/Border.cs
+++ Assets/Scripts/Border.cs
@@ -2,18 +2,18 @@
 
 public class Border
 {
-	private LineRenderer lineRenderer;
-	private EdgeCollider2D edge;
-
+	private LineRenderer _lineRenderer;
+	private EdgeCollider2D _edge;
+	
 	public LineRenderer LineRend
 	{
 		get
 		{
-			return lineRenderer;
+			return LineRenderer;
 		}
 		set
 		{
-			lineRenderer = value;
+			LineRenderer = value;
 		}
 	}
 
@@ -21,11 +21,37 @@ public class Border
 	{
 		get
 		{
-			return edge;
+			return Edge1;
 		}
 		set
 		{
-			edge = value;
+			Edge1 = value;
+		}
+	}
+
+	public LineRenderer LineRenderer
+	{
+		get
+		{
+			return _lineRenderer;
+		}
+
+		set
+		{
+			this._lineRenderer = value;
+		}
+	}
+
+	public EdgeCollider2D Edge1
+	{
+		get
+		{
+			return _edge;
+		}
+
+		set
+		{
+			this._edge = value;
 		}
 	}
 }
--- Assets/Scripts/BorderController.cs
+++ Assets/Scripts/BorderController.cs
@@ -12,57 +12,57 @@ public class BorderController : MonoBehaviour
 	private Border leftBorder = new Border();
 	private Border rightBorder = new Border();
 
-	 private void Start ()
+	private void Start()
 	{
-		AssignEdgeRanderer ();
-		if ( randomizeOnStart )
+		AssignEdgeRanderer();
+		if (randomizeOnStart)
 		{
-			UpdateLine ();
+			UpdateLine();
 		}
-		GenerateBorder ();
+		GenerateBorder();
 	}
-	
-	public void UpdateLine ()
+
+	public void UpdateLine()
 	{
 		line.points = new Vector3[size];
 		Vector3 v = Vector3.zero;
-		for ( int i=0; i < line.PointCount; i++ )
+		for (int i = 0; i < line.PointCount; i++)
 		{
 			v.y += steps;
-			v.x += Random.Range ( -randomX, randomX );
+			v.x += Random.Range(-randomX, randomX);
 			line.points[i] = v;
 		}
 	}
 
-	public void GenerateBorder ()
+	public void GenerateBorder()
 	{
-		AssignEdgeRanderer ();
-		leftBorder.LineRend = LineToLineRenderer ( line.points, leftBorder.LineRend );
-		rightBorder.LineRend = LineToLineRenderer ( line.points, rightBorder.LineRend );
-		leftBorder.Edge = LineToEdge ( line.points, leftBorder.Edge );
-		rightBorder.Edge = LineToEdge ( line.points, rightBorder.Edge );
+		AssignEdgeRanderer();
+		leftBorder.LineRend = LineToLineRenderer(line.points, leftBorder.LineRend);
+		rightBorder.LineRend = LineToLineRenderer(line.points, rightBorder.LineRend);
+		leftBorder.Edge = LineToEdge(line.points, leftBorder.Edge);
+		rightBorder.Edge = LineToEdge(line.points, rightBorder.Edge);
 	}
 
-	public LineRenderer LineToLineRenderer ( Vector3[] points, LineRenderer render )
+	public LineRenderer LineToLineRenderer(Vector3[] points, LineRenderer render)
 	{
-		render.SetVertexCount ( points.Length );
-		render.SetPositions ( points );
+		render.SetVertexCount(points.Length);
+		render.SetPositions(points);
 		return render;
 	}
 
-	public EdgeCollider2D LineToEdge ( Vector3[] points, EdgeCollider2D edge )
+	public EdgeCollider2D LineToEdge(Vector3[] points, EdgeCollider2D edge)
 	{
-		edge.points = points.ToVector2 ();
+		edge.points = points.ToVector2();
 		return edge;
 	}
 
-	private void AssignEdgeRanderer ()
+	private void AssignEdgeRanderer()
 	{
-		Transform left = this.transform.FindChild ( "LeftLine" );
-		Transform right = this.transform.FindChild ( "RightLine" );
-		leftBorder.LineRend = left.GetComponent<LineRenderer> ();
-		rightBorder.LineRend = right.GetComponent<LineRenderer> ();
-		leftBorder.Edge = left.GetComponent<EdgeCollider2D> ();
-		rightBorder.Edge = right.GetComponent<EdgeCollider2D> ();
+		Transform left = this.transform.FindChild("LeftLine");
+		Transform right = this.transform.FindChild("RightLine");
+		leftBorder.LineRend = left.GetComponent<LineRenderer>();
+		rightBorder.LineRend = right.GetComponent<LineRenderer>();
+		leftBorder.Edge = left.GetComponent<EdgeCollider2D>();
+		rightBorder.Edge = right.GetComponent<EdgeCollider2D>();
 	}
 }
