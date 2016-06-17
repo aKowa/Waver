04872e2d41e2a9e3278da27847c82cd21dbe6d5a
Andre Kowalewski <a.kowa@gmx.de>
2016-06-17 19:53:18 +0200

    [STARTED] audio strem test

---
+++ Assets/StreamTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class StreamTest : MonoBehaviour
+{
+	private AudioSource source;
+
+	private void Start ()
+	{
+		source = GetComponent<AudioSource>();
+		if (source.clip != null)
+		{
+			source.Play();
+		}
+	}
+}
